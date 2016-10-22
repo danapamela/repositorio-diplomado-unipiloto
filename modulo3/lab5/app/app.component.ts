24ddc2b783a714254bda0d5ecb413524c3ac95d2
Dana Camelo <danita_941230@hotmail.com>
2016-10-21 21:53:22 -0500

    update modulo3

---
+++ modulo3/lab5/app/app.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component } from '@angular/core';
+
+ export class ride {
+    airport: string;
+    app: string;
+    units: number ;
+    features: string;
+    default: string;
+    
+ }
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/templates/ride.html'
+})
+export class AppComponent {
+
+    title: string = "carrera";
+    selected: ride;
+    products: ride[] = ;
+
+    onSelect(product: Product) {
+        this.selected = product;
+    }
+}
+
